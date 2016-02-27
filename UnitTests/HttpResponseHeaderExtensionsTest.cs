ded4184772688c732bee9d7b5f31aa8861383dab
Bastian Eicher <bastian@eicher.net>
2016-02-27 17:01:59 +0100

    Added unit tests

---
+++ UnitTests/HttpResponseHeaderExtensionsTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace WebApi.LinkHeader
+{
+    [TestFixture]
+    public class HttpResponseHeaderExtensionsTest
+    {
+        [Test]
+        public void TestAddLink()
+        {
+            var headers = new HttpResponseMessage().Headers;
+            headers.AddLink(new Uri("http://myuri/123"), rel: "my-rel", title: "Title");
+
+            string linkHeader = headers.GetValues("Link").First();
+            linkHeader.Should().Be("<http://myuri/123>; rel=my-rel; title=Title");
+        }
+
+        [Test]
+        public void TestAddLinkTemplate()
+        {
+            var headers = new HttpResponseMessage().Headers;
+            headers.AddLink(new Uri("http://myuri/{id}"), rel: "my-rel", templated: true);
+
+            string linkHeader = headers.GetValues("Link").First();
+            linkHeader.Should().Be("<http://myuri/{id}>; rel=my-rel; templated=true");
+        }
+    }
+}
